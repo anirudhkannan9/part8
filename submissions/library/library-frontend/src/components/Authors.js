5975b5589244590b251807dd2b04743787b449d1
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-08-04 22:33:37 -0700

    Ex 8.11: implemented setting/editing birthyear of an Author via the frontend (with GraphQL mutations). Implemented keeping 'Authors' view up to date with new birthyear by refecting ALL_AUTHORS query.

--- submissions/library/library-frontend/src/components/Authors.js
+++ submissions/library/library-frontend/src/components/Authors.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { SET_BIRTHYEAR } from '../mutations'
+import { ALL_AUTHORS } from '../queries'
 
 const Authors = (props) => {
+  const [ name, setName ] = useState('')
+  const [ born, setBorn ] = useState('')
+
+  const [ setBirthyear ] = useMutation(SET_BIRTHYEAR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
   if (!props.show) {
     return null
   } else if (!props.authors) {
@@ -8,6 +18,18 @@ const Authors = (props) => {
   } else {
 
     let authors = props.authors.allAuthors
+
+
+    const handleSubmit = async event => {
+      event.preventDefault()
+    
+      let bornInt = parseInt(born)
+      setBirthyear({ variables: { name, setBornTo: bornInt }})
+
+
+      setName('')
+      setBorn('')
+    }
 
     return (
       <div>
@@ -32,6 +54,26 @@ const Authors = (props) => {
             )}
           </tbody>
         </table>
+
+        <br></br>
+
+        <h2>Set birthyear</h2>
+
+        <form onSubmit={ handleSubmit }>
+          name: 
+          <input
+            value={ name }
+            onChange={ ({ target }) => setName(target.value)}
+          />
+          <br></br>
+          born:
+          <input
+            value={ born }
+            onChange={ ({ target }) => setBorn(target.value)}
+          />
+          <br></br>
+          <button type='submit'>update author</button>
+        </form>
   
       </div>
     )
